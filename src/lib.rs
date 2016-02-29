72ce7cdc9cb4123d325143e416d5f76cf7771bd3
xenor <xenorlawl@gmail.com>
2016-02-29 23:36:03 +0100

    lol

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+extern crate chrono;
+extern crate time;
+extern crate crypto;
+extern crate rustc_serialize;
+extern crate rand;
+extern crate sodiumoxide;
+extern crate lzma;
+
+pub mod ed25519;
+pub mod meta;
+pub mod signature;
+pub mod certificate;
