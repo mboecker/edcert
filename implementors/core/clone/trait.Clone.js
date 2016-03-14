e73ad18cf5acafd7628f878f32b7a8489cab3d12
xenor <xenorlawl@gmail.com>
2016-03-15 00:46:41 +0100

    real docs this time

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['edcert'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='edcert/meta/struct.Meta.html' title='edcert::meta::Meta'>Meta</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='edcert/signature/struct.Signature.html' title='edcert::signature::Signature'>Signature</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='edcert/certificate/struct.Certificate.html' title='edcert::certificate::Certificate'>Certificate</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
