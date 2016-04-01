46764f74e3228197ea2e613717bf817b0ec5f1e5
xenor <xenorlawl@gmail.com>
2016-04-01 20:31:12 +0200

    formatted ed25519 test vectors

--- src/ed25519.rs
+++ src/ed25519.rs
@@ -156,15 +156,14 @@ fn test_testvectors() {
     let pbl = decode("77f48b59caeda77751ed138b0ec667ff50f8768c25d48309a8f386a2bad187fb");
 
     let msg = decode("916c7d1d268fc0e77c1bef238432573c39be577bbea099893\
-                                              6add2b50a653171ce18a542b0b7f96c1691a3be6031522894a8\
-                                              634183eda38798a0c5d5d79fbd01dd04a8646d71873b77b2219\
-                                              98a81922d8105f892316369d5224c9983372d2313c6b1f4556e\
-                                              a26ba49d46e8b561e0fc76633ac9766e68e21fba7edca93c4c7\
-                                              460376d7f3ac22ff372c18f613f2ae2e856af40");
+                      6add2b50a653171ce18a542b0b7f96c1691a3be6031522894a8\
+                      634183eda38798a0c5d5d79fbd01dd04a8646d71873b77b2219\
+                      98a81922d8105f892316369d5224c9983372d2313c6b1f4556e\
+                      a26ba49d46e8b561e0fc76633ac9766e68e21fba7edca93c4c7\
+                      460376d7f3ac22ff372c18f613f2ae2e856af40");
 
-    let sig = decode("6bd710a368c1249923fc7a1610747403040f0cc30815a00f9\
-                                              ff548a896bbda0b4eb2ca19ebcf917f0f34200a9edbad3901b6\
-                                              4ab09cc5ef7b9bcc3c40c0ff7509");
+    let sig = decode("6bd710a368c1249923fc7a1610747403040f0cc30815a00f9ff548a896bbda0b4eb2ca19ebc\
+                      f917f0f34200a9edbad3901b64ab09cc5ef7b9bcc3c40c0ff7509");
 
     let sig_my = sign(&msg, &prv);
 
